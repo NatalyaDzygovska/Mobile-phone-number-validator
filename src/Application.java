511fb0a5ad276612ecf95a4e0a99b270cd705c94
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-10-23 02:11:50 +0300

    Add calculation

--- src/Application.java
+++ src/Application.java
@@ -9,25 +9,61 @@ public class Application {
 
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = "";
-        boolean valid = true;
+        String phone = "";
+        boolean valid;
         do {
             System.out.println("Please, enter the phone number:");
             try {
-                str += bufferedReader.readLine();
+                phone += bufferedReader.readLine();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            valid = DumbValidation.isValid(str);
+            valid = DumbValidation.isValid(phone);
             if (valid) {
                 System.out.println("Phone number is correct!!!");
-            }else {
+            } else {
                 System.out.println("Phone number is incorrect");
+                phone = "";
             }
-            str = "";
         } while (!valid);
 
+        int sum = getSum(phone);
 
+        printSum(sum);
+    }
 
+    private static void printSum(int sum) {
+        switch (sum) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            default:
+                System.out.println(sum);
+        }
+    }
+
+    static int round = 1;
+
+    private static int getSum(String phone) {
+        int sum = 0;
+        for (int i = 0; i < phone.length(); i++) {
+            sum += Character.getNumericValue(phone.charAt(i));
+        }
+        System.out.println(round + "st round of calculation, sum is " + sum);
+        if (sum > 9) {
+            round++;
+            String sumRow = Integer.toString(sum);
+            sum = getSum(sumRow);
+        }
+        return sum;
     }
 }
