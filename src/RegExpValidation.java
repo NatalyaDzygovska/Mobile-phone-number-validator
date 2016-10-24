706967b6a8d7cc2812a9642804b7da7818227ad4
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-10-24 22:07:35 +0300

    Add field "pattern" to RegExpValidation

--- src/RegExpValidation.java
+++ src/RegExpValidation.java
@@ -2,8 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExpValidation {
+    public static String pattern = "^(0038|\\+38|8)?0(39|50|63|6[6-8]|9[1-9])\\d{7}$";
+
     public static boolean isValid(String phone){
-        Pattern pattern = Pattern.compile("^(0038|\\+38|8)?0(39|50|63|6[6-8]|9[1-9])\\d{7}$");
+        Pattern pattern = Pattern.compile(RegExpValidation.pattern);
         Matcher matcher = pattern.matcher(phone);
         return matcher.matches();
     }
