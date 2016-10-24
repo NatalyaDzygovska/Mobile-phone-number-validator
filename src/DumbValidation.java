50b31909a850330aee06562164a90a1a9ea7f15b
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-10-24 22:00:46 +0300

    Add method isCorrectCode into DumbValidation

--- src/DumbValidation.java
+++ src/DumbValidation.java
@@ -5,50 +5,36 @@ public class DumbValidation {
     private static String[] operatorCodes =
             {"39", "50", "63", "66", "67", "68", "91", "92", "93", "94", "95", "96", "97", "98", "99"};
 
-    public static boolean isValid(String str) {
-        if (str.substring(0, 2).equals("00")) {
-            if (str.substring(2, 5).equals("380")) {
-                boolean isCorrectCode = false;
-                for (String code : operatorCodes) {
-                    if (str.substring(5, 7).equals(code)) {
-                        isCorrectCode = true;
-                    }
-                }
-                if (isCorrectCode && str.length()==14) {
+    public static boolean isValid(String phone) {
+        if (phone.substring(0, 2).equals("00")) {
+            if (phone.substring(2, 5).equals("380")) {
+                if (isCorrectCode(phone.substring(5, 7)) && phone.length() == 14) {
                     return true;
                 }
             }
-        } else if (str.charAt(0) == '0' && str.charAt(1) != 0) {
-            boolean isCorrectCode = false;
-            for (String code : operatorCodes) {
-                if (str.substring(1, 3).equals(code)) {
-                    isCorrectCode = true;
-                }
-            }
-            if (isCorrectCode && str.length()==10) {
+        } else if (phone.charAt(0) == '0' && phone.charAt(1) != 0) {
+            if (isCorrectCode(phone.substring(1, 3)) && phone.length() == 10) {
                 return true;
             }
-        } else if (str.charAt(0) == '+' && str.substring(1, 4).equals("380")) {
-            boolean isCorrectCode = false;
-            for (String code : operatorCodes) {
-                if (str.substring(4, 6).equals(code)) {
-                    isCorrectCode = true;
-                }
-            }
-            if (isCorrectCode && str.length()==13) {
+        } else if (phone.charAt(0) == '+' && phone.substring(1, 4).equals("380")) {
+            if (isCorrectCode(phone.substring(4, 6)) && phone.length() == 13) {
                 return true;
             }
-        }else if (str.charAt(0)=='8' && str.charAt(1)=='0'){
-            boolean isCorrectCode = false;
-            for (String code : operatorCodes) {
-                if (str.substring(2, 4).equals(code)) {
-                    isCorrectCode = true;
-                }
-            }
-            if (isCorrectCode && str.length()==11) {
+        } else if (phone.charAt(0) == '8' && phone.charAt(1) == '0') {
+            if (isCorrectCode(phone.substring(2, 4)) && phone.length() == 11) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static boolean isCorrectCode(String enteredCode) {
+        boolean isCorrectCode = false;
+        for (String code : operatorCodes) {
+            if (enteredCode.equals(code)) {
+                isCorrectCode = true;
+            }
+        }
+        return isCorrectCode;
     }
 }
