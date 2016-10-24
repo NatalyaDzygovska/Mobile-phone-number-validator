af0a66bcec09118c800c7de22164021e8f034e33
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-10-24 22:51:34 +0300

    Edit field "pattern" in RegExpValidation

--- src/RegExpValidation.java
+++ src/RegExpValidation.java
@@ -2,10 +2,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExpValidation {
-    public static String pattern = "^(0038|\\+38|8)?0(39|50|63|6[6-8]|9[1-9])\\d{7}$";
+    private static Pattern pattern = Pattern.compile("^(0038|\\+38|8)?0(39|50|63|6[6-8]|9[1-9])\\d{7}$");
 
     public static boolean isValid(String phone){
-        Pattern pattern = Pattern.compile(RegExpValidation.pattern);
         Matcher matcher = pattern.matcher(phone);
         return matcher.matches();
     }
